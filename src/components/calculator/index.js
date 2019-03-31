377ad54c56b8e4baf540b51db8dde6fb5c86e19f
Giacomo Sorbi <giacomosorbi@Ajnaputer-2.local>
2019-03-31 21:28:39 +0100

    button area created, tests added

--- src/components/calculator/index.js
+++ src/components/calculator/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './calculator.css'
 import Display from '../display'
+import Button from '../button'
+import { BASE_LAYOUT } from '../constants'
 
-const Calculator = props => (
-  <div className='calculator-container'>
-    <Display displayedText={'3.14'} />
-  </div>
-)
+const Calculator = props => {
+  const onClickHandler = event => console.log(event.target.innerText)
+  return (
+    <div className='calculator-container'>
+      <Display displayedText={'3.14'} />
+      <div className='calculator-buttons'>
+        {BASE_LAYOUT.map(btn => (
+          <Button key={btn.text} onClick={onClickHandler}>
+            {btn.text}
+          </Button>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default Calculator
---
+++ src/components/constants/index.js
@@ -0,0 +1,18 @@
+export const BASE_LAYOUT = [
+  { text: '7' },
+  { text: '8' },
+  { text: '9' },
+  { text: '/' },
+  { text: '4' },
+  { text: '5' },
+  { text: '6' },
+  { text: '*' },
+  { text: '1' },
+  { text: '2' },
+  { text: '3' },
+  { text: '-' },
+  { text: '.' },
+  { text: '0' },
+  { text: '=' },
+  { text: '+' },
+]
