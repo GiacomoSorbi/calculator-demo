c88462eeab870df18d869b7bab475a9d597e41b6
Giacomo Sorbi <giacomosorbi@Ajnaputer-2.local>
2019-03-31 22:22:24 +0100

    Updates

---
+++ precache-manifest.7f04007b18bc17024bc74b024fec278d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d98b228757aabc0869a",
+    "url": "/static/css/main.27856939.chunk.css"
+  },
+  {
+    "revision": "3d98b228757aabc0869a",
+    "url": "/static/js/main.858a46f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3824e11d21c245d864f9",
+    "url": "/static/js/2.18326d6d.chunk.js"
+  },
+  {
+    "revision": "d98a6a66fa22ecfe00fd041d16ed1607",
+    "url": "/static/media/logo.d98a6a66.svg"
+  },
+  {
+    "revision": "974e5277a8226d48521cd237c87933e6",
+    "url": "/static/media/pocket_calcuatlor_ot.974e5277.otf"
+  },
+  {
+    "revision": "7abdb54956012fd597a0ac96ae171df7",
+    "url": "/index.html"
+  }
+];
